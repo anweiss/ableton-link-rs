e40aa65dae3ad7edc6495ea7cd3c1ec97e13a584
anweiss <2326106+anweiss@users.noreply.github.com>
2023-12-01 12:58:59 -0500

    updates

--- src/discovery/mod.rs
+++ src/discovery/mod.rs
@@ -13,7 +13,7 @@ pub const ENCODING_CONFIG: Configuration<BigEndian, Fixint> = bincode::config::s
     .with_big_endian()
     .with_fixed_int_encoding();
 
-pub const UNICAST_IP_ANY: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(192, 168, 1, 23), 0);
+pub const UNICAST_IP_ANY: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0);
 pub const MULTICAST_IP_ANY: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 20808);
 
 pub const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 76, 78, 75);
