0bd9c1eadae9434ca38d7db896f32551788cc3c4
anweiss <2326106+anweiss@users.noreply.github.com>
2023-12-07 13:13:15 -0500

    dynamic IP selection

--- src/discovery/mod.rs
+++ src/discovery/mod.rs
@@ -13,7 +13,6 @@ pub const ENCODING_CONFIG: Configuration<BigEndian, Fixint> = bincode::config::s
     .with_big_endian()
     .with_fixed_int_encoding();
 
-pub const MULTICAST_IP_ANY: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 20808);
-
-pub const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 76, 78, 75);
 pub const LINK_PORT: u16 = 20808;
+pub const MULTICAST_IP_ANY: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), LINK_PORT);
+pub const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 76, 78, 75);
